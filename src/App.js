f39a3de0f348c9b1d4722128b9f38bc91252d547
Vishwa Derasariya <vishwaderasariya@gmail.com>
2020-04-13 19:46:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Keypad from './component/Keypad';
+import Result from './component/Result';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       result : ""
+    }
+  }
+   
+  onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+  }
+  calculate = () =>
+  {
+    try{
+      this.setState({
+        result : (eval(this.state.result || "")) 
+      })
+    }
+      catch(e){
+        this.setState({
+          result : "ERROR OCCURED"
+        })
+      }
+    
+  }
+  reset = () =>
+  {
+    this.setState({
+      result : ""
+    })
+
+  }
+  escape = () =>
+  {
+    this.setState({
+      result : this.state.result.slice(0,-1)
+    })
+  }
+
+  render()
+  {
+    return(
+      <div className='calculator'>
+        <Result result={this.state.result}/>
+        <Keypad onClick={this.onClick}/>
+      </div>
+    )
+  }
+}
+export default App
